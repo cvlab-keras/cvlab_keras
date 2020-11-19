d6c41697d9fff14d8a836cbe8a40079b5169e06e
filip-chodziutko <62184852+filip-chodziutko@users.noreply.github.com>
2020-11-19 12:28:27 +0100

    Add model preview (#5)

--- __init__.py
+++ __init__.py
@@ -1,3 +1,15 @@
+import tensorflow as tf
+
 from cvlab.diagram.elements import load_auto
+from cvlab.view.widgets import OutputPreview
+from cvlab_keras.model_utils import set_model
+
+
+# limit gpu usage for tensorflow
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
+
+# add preview for model class
+OutputPreview.preview_callbacks.append((tf.keras.models.Model, set_model))
 
 load_auto(__file__)
