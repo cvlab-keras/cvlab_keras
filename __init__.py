d5a2190176754763643171415434f5ab361b7b6b
filip-chodziutko <62184852+filip-chodziutko@users.noreply.github.com>
2020-11-26 17:29:06 +0100

    Generalize GPUs usage limitation (#8)

--- __init__.py
+++ __init__.py
@@ -4,10 +4,18 @@ from cvlab.diagram.elements import load_auto
 from cvlab.view.widgets import OutputPreview
 from cvlab_keras.model_utils import set_model
 
-
 # limit gpu usage for tensorflow
 gpus = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(gpus[0], True)
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
 
 # add preview for model class
 OutputPreview.preview_callbacks.append((tf.keras.models.Model, set_model))
