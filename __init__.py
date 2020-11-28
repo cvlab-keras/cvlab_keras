b7f0f76439cf297b045102613cbb9f0f7f7a829d
filip-chodziutko <filipchodziutko@gmail.com>
2020-11-28 17:18:30 +0100

    Use provided method for adding samples

--- __init__.py
+++ __init__.py
@@ -1,11 +1,8 @@
 import tensorflow as tf
 
-from glob import glob
-
 from cvlab.diagram.elements import load_auto
 from cvlab.view.widgets import OutputPreview
-from cvlab_samples import OpenExampleAction, get_menu
-from cvlab.diagram.elements import add_plugin_callback
+from cvlab_samples import add_samples_submenu
 
 from cvlab_keras.model_utils import set_model
 from cvlab_keras.shared import SAMPLES_DIR
@@ -20,20 +17,5 @@ OutputPreview.preview_callbacks.append((tf.keras.models.Model, set_model))
 # load elements
 load_auto(__file__)
 
-
-def add_samples(main_window):
-    menu_name = "Examples"
-    submenu_name = "Keras"
-    menu_title = menu_name + "/" + submenu_name
-    samples = glob(SAMPLES_DIR + "/*.cvlab")
-    samples.sort()
-
-    print("Adding {} sample diagrams to '{}' menu".format(len(samples), menu_title))
-
-    menu = get_menu(main_window, menu_title)
-
-    for sample in samples:
-        menu.addAction(OpenExampleAction(main_window, sample))
-
-
-add_plugin_callback(add_samples)
+# load example diagrams
+add_samples_submenu('Keras', SAMPLES_DIR)
