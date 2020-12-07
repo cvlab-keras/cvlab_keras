22a9dc343f37fb5c01c3ab72d8109815821c553e
filip-chodziutko <62184852+filip-chodziutko@users.noreply.github.com>
2020-12-07 18:12:20 +0100

    Add sample diagrams loading

--- __init__.py
+++ __init__.py
@@ -2,7 +2,10 @@ import tensorflow as tf
 
 from cvlab.diagram.elements import load_auto
 from cvlab.view.widgets import OutputPreview
+from cvlab_samples import add_samples_submenu
+
 from cvlab_keras.model_utils import set_model
+from cvlab_keras.shared import SAMPLES_DIR
 
 # limit gpu usage for tensorflow
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -20,4 +23,8 @@ if gpus:
 # add preview for model class
 OutputPreview.preview_callbacks.append((tf.keras.models.Model, set_model))
 
+# load elements
 load_auto(__file__)
+
+# load example diagrams
+add_samples_submenu('Keras', SAMPLES_DIR)
